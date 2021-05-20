e39a9e787693b9ae63514d220c7adc98545be99d
Rayza Oliveira <rayza.ocr@gmail.com>
2021-05-20 20:35:04 -0300

    feat: Adjust backend project

--- modulo-1/backend/src/index.js
+++ modulo-1/backend/src/index.js
@@ -1,12 +1,14 @@
-const express = require('express');    
+const express = require('express');
+const cors = require('cors');
 
-// Criando estrutura 
+// Criando estrutura
 const { isUuid, uuid } = require('uuidv4');
 // const { request } = require('express');
 
 //iniciando o App
-const app = express ();
+const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 /**
@@ -17,109 +19,108 @@ app.use(express.json());
  * DELETE: Deletar uma informação no back-end
  */
 
- /**
-  * Tipos de parêmetros:
-  * 
-  * Querry Params: Filtros e paginação
-  * Route Params:  Identificar recursos (Atualizar/Deletar)
-  * Request Body: Conteúdo na hora criar ou editar um recurso (JSON)
-  * 
-  */
+/**
+ * Tipos de parêmetros:
+ *
+ * Querry Params: Filtros e paginação
+ * Route Params:  Identificar recursos (Atualizar/Deletar)
+ * Request Body: Conteúdo na hora criar ou editar um recurso (JSON)
+ *
+ */
 
-  /**
-   * Middleware: 
-   * 
-   * Intercepatador de requisições: que interromper totalmente a requisição ou alterar dados da requisições.
-   * 
-   */
+/**
+ * Middleware:
+ *
+ * Intercepatador de requisições: que interromper totalmente a requisição ou alterar dados da requisições.
+ *
+ */
 
-  const projects = [];
+const projects = [];
 
 function logRequests(request, response, next) {
-    const {method, url } = request;
+  const { method, url } = request;
 
-    const logLabel = `[${method.toUpperCase()}] ${url}`;
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
 
-    // console.log('1');
-    console.time(logLabel);   
+  // console.log('1');
+  console.time(logLabel);
 
-    next();   //Próximo middleware
+  next(); //Próximo middleware
 
-    // console.log ('2');
-    console.timeEnd(logLabel);
+  // console.log ('2');
+  console.timeEnd(logLabel);
 }
 
 function validadeProjectId(request, response, next) {
-    const { id } = request.params; 
+  const { id } = request.params;
 
-    if (!isUuid(id)) {
-        return response.status(400).json({ error: 'Invalide project ID'});
-    }
-    return next();
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalide project ID' });
+  }
+  return next();
 }
 
 app.use(logRequests);
 app.use('/projects/:id', validadeProjectId);
 
-//Primeira Rota 
+//Primeira Rota
 //'/projects' : rota raiz, request e response: parametros requerimento e resposta
-app.get('/projects', (request, response) => {       
-    // console.log ('3');                      
-    const { title} = request.query;
+app.get('/projects', (request, response) => {
+  // console.log ('3');
+  const { title } = request.query;
 
-    const results = title
-    ? projects.filter(project => project.title.includes(title))
+  const results = title
+    ? projects.filter((project) => project.title.includes(title))
     : projects;
 
-    return response.json(results); 
+  return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
-    const {title,owner} = request.body; 
- 
-    const project  = {id: uuid(), title, owner};
+  const { title, owner } = request.body;
 
-    projects.push(project);
+  const project = { id: uuid(), title, owner };
 
-    return response.json(project);
+  projects.push(project);
+
+  return response.json(project);
 });
 
-app.put('/projects/:id',(request, response) => {
-    const {id} = request.params;
-    const {title,owner} = request.body;
+app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
 
-    const projectIndex = projects.findIndex(project => project.id == id);
+  const projectIndex = projects.findIndex((project) => project.id == id);
 
-    if (projectIndex < 0) {
-        return response.status(400).json({ error: 'Project not found.'})
-    }
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.' });
+  }
 
-    const project = {
-        id,
-        title,
-        owner,
-    };
+  const project = {
+    id,
+    title,
+    owner,
+  };
 
-    projects [projectIndex] = project;
+  projects[projectIndex] = project;
 
-    return response.json(project);
+  return response.json(project);
 });
 
-app.delete('/projects/:id',(request, response) => {
-    const { id } = request.params;
+app.delete('/projects/:id', (request, response) => {
+  const { id } = request.params;
 
-    const projectIndex = projects.findIndex(project => project.id == id);
+  const projectIndex = projects.findIndex((project) => project.id == id);
 
-    if (projectIndex < 0) {
-        return response.status(400).json({ error: 'Project not found.'})
-    }
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.' });
+  }
 
-    projects.splice(projectIndex, 1);
+  projects.splice(projectIndex, 1);
 
-    return response.status(204).send();
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
-    console.log('Back-end started ❤');
+  console.log('Back-end started ❤');
 });
-
