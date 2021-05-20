d2424fa8722b63999bd8ef869e8706c53d5c81bb
Rayza Oliveira <rayza.ocr@gmail.com>
2021-05-20 20:35:37 -0300

    feat: update frontend project

--- modulo-1/frontend/src/App.js
+++ modulo-1/frontend/src/App.js
@@ -1,29 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './App.css';
+import background from './assests/background.jpg';
 
 import Header from './components/Header';
 
-
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+  //UseState retorna um array com 2 posições
+  //1. Variável com o seu valor inicial , project
+  // 2. Funcao para utilizar esse valor , setProjects
 
+  const [projects, setProjects] = useState([]);
 
-  function handleAddProject() {
-    //projects.push(`Novo projeto ${Date.now}`); 
+  // useEffect, dispar funcoes
+  // 1 parametro . qual funcao quero disparar?
+  // 2 parametro. qdo quero dispar?
+  useEffect(() => {
+    api.get('projects').then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
 
-    setProjects([...projects, `Novoprojeto ${Date.now()}`]);
+  async function handleAddProject() {
+    //.push altera o project e nao criar um novo valor
+    //projects.push(`Novo projeto ${Date.now}`);
 
+    // criar um novo array
+    // setProjects([...projects, `Novoprojeto ${Date.now()}`]);
+
+    const response = await api.post('projects', {
+      title: `Novo projeto 🎋 ${Date.now()}`,
+      owner: 'Rayza Oliveira',
+    });
+
+    const project = response.data;
+    setProjects([...projects, project]);
   }
 
   return (
     <>
-      <Header title="Projects"/>
+      <Header title="Projects" />
+      <img width={300} src={background} />
+
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map((project) => (
+          <li key={project.id}>{project.title}</li>
+        ))}
       </ul>
 
-      <button type = "button" onClick={handleAddProject}>Adicinar Projeto</button>
+      <button type="button" onClick={handleAddProject}>
+        Adicinar Projeto
+      </button>
     </>
   );
 }
 
 export default App;
+
+// Component
+// Propriedade
+// Estado e Imutabilidade
--- modulo-1/frontend/webpack.config.js
+++ modulo-1/frontend/webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry:path.resolve(__dirname, 'src', 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
@@ -18,8 +18,19 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-        }
-      }
-    ]
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: {
+          loader: 'file-loader',
+        },
+      },
+    ],
   },
 };
