ae16603b94eeb639019fd8a9aad8536725dda379
Rayza Oliveira <rayza.ocr@gmail.com>
2021-05-22 21:35:53 -0300

    feat: creating new routes

---
+++ nivel-2/primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -0,0 +1,23 @@
+import {Router} from 'express';
+import {v4 as uuid} from 'uuid';
+
+const appointmentsRouter = Router();
+
+const appointments = [];
+
+// POST http://localhost:3333/appintments
+appointmentsRouter.post('/', (request, response) => {
+  const {provider, date} = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
