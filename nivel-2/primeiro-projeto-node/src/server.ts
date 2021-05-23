7fb8c72a0956c2d7713c0c25d6ec30bf96e422fa
Rayza Oliveira <rayza.ocr@gmail.com>
2021-05-22 21:36:19 -0300

    refactor: refactoring server to add routes

--- nivel-2/primeiro-projeto-node/src/server.ts
+++ nivel-2/primeiro-projeto-node/src/server.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello Rayza 🔥" });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log("❤Server started on port 3333!❤");
+  console.log('❤Server started on port 3333!❤');
 });
